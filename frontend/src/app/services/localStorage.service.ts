8b0aa7b0a3940679588211c459b198cc433cfed0
pedro <pedro.jcaceresl@gmail.com>
2022-11-20 13:26:45 -0300

    Added method to retrieve data as JSON

--- frontend/src/app/services/localStorage.service.ts
+++ frontend/src/app/services/localStorage.service.ts
@@ -1,39 +1,33 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Usuario } from '../interfaces/Usuario';
-
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Usuario } from "../interfaces/Usuario";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LocalStorageService {
-  
+  constructor() {}
 
-
-  constructor() { }
-
-  public  getItem(key:string) {
-
-    let item =localStorage.getItem("key");
-    return item? JSON.parse(item):[];
-    
-  } 
-  
-  public  setItem(key:string,object:any) {
-    localStorage.setItem(key,JSON.stringify(object))
+  public getItem(key: string) {
+    let item = localStorage.getItem("key");
+    return item ? JSON.parse(item) : [];
   }
 
-  public  clearItem() {
+  public setItem(key: string, object: any) {
+    localStorage.setItem(key, JSON.stringify(object));
+  }
+
+  public clearItem() {
     localStorage.clear();
   }
 
-  public  removeItem(key:string) {
+  public removeItem(key: string) {
     localStorage.removeItem(key);
   }
 
-
-
- 
-
+  public getJSON(key: string) {
+    let item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : [];
+  }
 }
