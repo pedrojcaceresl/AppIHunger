7dcfc4d60f1f0c92c75c41ce808207d155712329
pedro <pedro.jcaceresl@gmail.com>
2022-11-24 19:24:54 -0300

    Added models

--- backend/src/models/usuario.model.js
+++ backend/src/models/usuario.model.js
@@ -3,7 +3,7 @@ const { sequelize } = require('../services/bd.service');
 
 const UsuarioModel = sequelize.define('usuario', {
     // Model attributes are defined here
-    usu_codigo: {
+    usu_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
@@ -19,12 +19,22 @@ const UsuarioModel = sequelize.define('usuario', {
         allowNull: true
             // allowNull defaults to true
     },
+    usu_rol: {
+        type: DataTypes.STRING,
+        allowNull: true
+            // allowNull defaults to true
+    },
     usu_email: {
         type: DataTypes.STRING,
         allowNull: true
             // allowNull defaults to true
     },
     usu_password: {
+        type: DataTypes.STRING,
+        allowNull: true
+            // allowNull defaults to true
+    },
+    usu_token: {
         type: DataTypes.STRING,
         allowNull: true
             // allowNull defaults to true
@@ -39,10 +49,13 @@ const UsuarioModel = sequelize.define('usuario', {
         allowNull: true
             // allowNull defaults to true   
     },
-    usu_token: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    usu_documento: {
+        type: DataTypes.TEXT,
+        allowNull: true
+            // allowNull defaults to true   
+    }
+
+
 }, {
     // Other model options go here
     tableName: 'usuarios',
