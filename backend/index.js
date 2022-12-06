3adcde3c852a7c3aec9ff2b8e932078375f5f047
pedro <pedro.jcaceresl@gmail.com>
2022-12-05 22:36:24 -0300

    Added morgan with colored requests middleware

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,31 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const PORT = process.env.PORT;
 const io = new Server();
+const morgan = require("morgan");
+const chalk = require("chalk");
 
 const swaggerDocs = require("./src/helpers/swagger.js");
 
 // Defining Expresss app
 const app = express();
 
+const morganMiddleware = morgan(function (tokens, req, res) {
+  return [
+    "\n\n\n",
+    chalk.hex("#ff4757").bold("🍄  Morgan --> "),
+    chalk.hex("#34ace0").bold(tokens.method(req, res)),
+    chalk.hex("#ffb142").bold(tokens.status(req, res)),
+    chalk.hex("#ff5252").bold(tokens.url(req, res)),
+    chalk.hex("#2ed573").bold(tokens["response-time"](req, res) + " ms"),
+    chalk.hex("#f78fb3").bold("@ " + tokens.date(req, res)),
+    chalk.yellow(tokens["remote-addr"](req, res)),
+    chalk.hex("#fffa65").bold("from " + tokens.referrer(req, res)),
+    chalk.hex("#1e90ff")(tokens["user-agent"](req, res)),
+    "\n\n\n",
+  ].join(" ");
+});
+
+app.use(morganMiddleware);
 // Using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 // app.use(
