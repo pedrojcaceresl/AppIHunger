b0d1616e5d78b03a27eb681d3c2dc397d83c5632
pedro <pedro.jcaceresl@gmail.com>
2022-12-08 20:15:48 -0300

    Added my order list

--- frontend/src/app/admin/pedidos/pedidos.page.ts
+++ frontend/src/app/admin/pedidos/pedidos.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { PedidoService } from "./../../services/pedido.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pedidos',
-  templateUrl: './pedidos.page.html',
-  styleUrls: ['./pedidos.page.scss'],
+  selector: "app-pedidos",
+  templateUrl: "./pedidos.page.html",
+  styleUrls: ["./pedidos.page.scss"],
 })
 export class PedidosPage implements OnInit {
+  constructor(private service: PedidoService) {}
 
-  constructor() { }
-
+  ocultar = false;
+  detalles = [];
   ngOnInit() {
+    this.listar();
   }
 
+  listar() {
+    this.service.getAll().subscribe((data) => {
+      this.detalles = data.result;
+      if (this.detalles.length > 0) this.ocultar = true;
+      else this.ocultar = false;
+      console.log(data);
+    });
+  }
+  onClick(id) {
+    this.service.update(id).subscribe((data) => {
+      console.log(data);
+      this.listar();
+    });
+  }
 }
