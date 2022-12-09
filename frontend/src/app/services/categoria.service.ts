be498b2929201546d3a93cd5ad70bd9618502aeb
pedro <pedro.jcaceresl@gmail.com>
2022-12-08 22:19:05 -0300

    Added delete method

--- frontend/src/app/services/categoria.service.ts
+++ frontend/src/app/services/categoria.service.ts
@@ -1,45 +1,47 @@
-import { Categoria } from './../interfaces/Categoria';
-import { LocalStorageService } from './localStorage.service';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Categoria } from "./../interfaces/Categoria";
+import { LocalStorageService } from "./localStorage.service";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoriaService {
-
   endPoint = environment.url;
   //headers ={"x-token":this.LocalStorageService.getString(environment.tokenKey)};
-  constructor(public http: HttpClient, private LocalStorageService:LocalStorageService) { }
+  constructor(
+    public http: HttpClient,
+    private LocalStorageService: LocalStorageService
+  ) {}
 
-  public get(): Observable<any>{
-    
+  public get(): Observable<any> {
     return this.http.get(`${this.endPoint}/categorias/get`);
   }
 
-  public filtrarCategoria(text: String): Observable<any>{
-    
+  public filtrarCategoria(text: String): Observable<any> {
     return this.http.get(`${this.endPoint}/categorias/getFilter/` + text);
   }
 
-  public getCategoriaById(id: number): Observable<any>{
-    
+  public getCategoriaById(id: number): Observable<any> {
     return this.http.get(`${this.endPoint}/categorias/getById/` + id);
   }
 
-  public crearCategoria(data: Observable<Categoria[]>): Observable<any>{
-    
+  public crearCategoria(data: Observable<Categoria[]>): Observable<any> {
     return this.http.post<Categoria[]>(`${this.endPoint}/categorias/add`, data);
   }
 
-  public actualizarCategoria(data: Observable<Categoria[]>): Observable<any>{
-    return this.http.put<Categoria[]>(`${this.endPoint}/categorias/update`, data);
+  public actualizarCategoria(data: Observable<Categoria[]>): Observable<any> {
+    return this.http.put<Categoria[]>(
+      `${this.endPoint}/categorias/update`,
+      data
+    );
   }
 
-/*   public eliminarCategoria(id: Observable<Categoria[]>) {
-    return this.http.delete<Categoria[]>(`${this.endPoint}/categorias/delete/` + id);
-  } */
-
+  public eliminarCategoria(id: Observable<Categoria[]>) {
+    return this.http.delete<Categoria[]>(
+      `${this.endPoint}/categorias/delete/` + id
+    );
+  }
 }
