2ab58fb0be56b087e07dc5c4c5a90bc583d5afb0
pedro <pedro.jcaceresl@gmail.com>
2022-12-09 18:36:31 -0300

    CRUD usuario

--- frontend/src/app/admin/usuarios/crear-usuario/crear-usuario.page.ts
+++ frontend/src/app/admin/usuarios/crear-usuario/crear-usuario.page.ts
@@ -1,49 +1,52 @@
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { UsuarioService } from './../../../services/usuario.service';
-import { Component, OnInit } from '@angular/core';
+import { AlertController } from "@ionic/angular";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { UsuarioService } from "./../../../services/usuario.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-crear-usuario',
-  templateUrl: './crear-usuario.page.html',
-  styleUrls: ['./crear-usuario.page.scss'],
+  selector: "app-crear-usuario",
+  templateUrl: "./crear-usuario.page.html",
+  styleUrls: ["./crear-usuario.page.scss"],
 })
 export class CrearUsuarioPage implements OnInit {
-
   usuario;
   crearUsuarioForm: FormGroup;
 
   constructor(
     private usuarioService: UsuarioService,
     private formBuilder: FormBuilder,
-    private router: Router
-    ) {
-
-      this.crearUsuarioForm = this.formBuilder.group({
-        usu_nombre: [''],
-        usu_email: [''],
-        usu_telefono: [''],
-        usu_password: [''],
-      })
-     }
-
-  ngOnInit() {
-  }
-
-  crearUsuario(){
-    console.log(this.crearUsuarioForm.value);
-    let usuario = this.crearUsuarioForm.value;
-    this.usuarioService.crearUsuario(usuario).subscribe((res) => { 
-      console.log(res);
-      this.goOut();
-     /*  if(res.success == true){
-        this.router.navigate(['/admin/usuarios']);
-      } */
+    private router: Router,
+    private alertController: AlertController
+  ) {
+    this.crearUsuarioForm = this.formBuilder.group({
+      usu_nombre: [""],
+      usu_email: [""],
+      usu_telefono: [""],
+      usu_imagen: [""],
+      usu_password: [""],
     });
   }
 
-  goOut(){
-    this.router.navigate(['/admin']);
-  }
+  ngOnInit() {}
 
+  async crearUsuario() {
+    console.log(this.crearUsuarioForm.value);
+    let usuario = this.crearUsuarioForm.value;
+    this.usuarioService.crearUsuario(usuario).subscribe((res: any) => {
+      console.log(res);
+      if (res.success == true) {
+        this.alertController
+          .create({
+            header: "Usuario Creado Exitosamente!",
+            buttons: ["Ok"],
+          })
+          .then((res) => {
+            res.present();
+          });
+        this.router.navigate(["/admin/usuarios"]);
+      }
+    });
+  }
+  prese;
 }
--- frontend/src/app/admin/usuarios/usuarios-routing.module.ts
+++ frontend/src/app/admin/usuarios/usuarios-routing.module.ts
@@ -1,25 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { UsuariosPage } from './usuarios.page';
+import { UsuariosPage } from "./usuarios.page";
 
 const routes: Routes = [
   {
-    path: '',
-    component: UsuariosPage
+    path: "",
+    component: UsuariosPage,
   },
   {
-    path: 'editar-usuario',
-    loadChildren: () => import('./editar-usuario/editar-usuario.module').then( m => m.EditarUsuarioPageModule)
+    path: "editar-usuario/:id",
+    loadChildren: () =>
+      import("./editar-usuario/editar-usuario.module").then(
+        (m) => m.EditarUsuarioPageModule
+      ),
   },
   {
-    path: 'crear-usuario',
-    loadChildren: () => import('./crear-usuario/crear-usuario.module').then( m => m.CrearUsuarioPageModule)
+    path: "crear-usuario",
+    loadChildren: () =>
+      import("./crear-usuario/crear-usuario.module").then(
+        (m) => m.CrearUsuarioPageModule
+      ),
   },
   {
-    path: 'usuario-modal',
-    loadChildren: () => import('./usuario-modal/usuario-modal.module').then( m => m.UsuarioModalPageModule)
-  }
+    path: "usuario-modal",
+    loadChildren: () =>
+      import("./usuario-modal/usuario-modal.module").then(
+        (m) => m.UsuarioModalPageModule
+      ),
+  },
 ];
 
 @NgModule({
